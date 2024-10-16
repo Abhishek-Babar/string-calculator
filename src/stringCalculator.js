d5be9c099eb5db2818f5c373cdbe08ed6cb70016
Abhishek-Babar <babarabhishek99@gmail.com>
2024-10-16 16:25:16 +0530

    Added step1 and step2 solution

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,7 @@
 function stringCalculator(input) {
     if (input === "") return 0;
+    const numbers = input.split(",").map(Number);
+    return numbers.reduce((sum, c) => sum + c , 0);
 }
 
 module.exports = stringCalculator;
