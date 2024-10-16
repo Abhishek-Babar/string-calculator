c28f5f72bf269b12eb650fcc737b865cd0942c28
Abhishek-Babar <babarabhishek99@gmail.com>
2024-10-16 16:43:57 +0530

    Added custom delimeter support solution

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,13 @@
 function stringCalculator(input) {
     if (input === "") return 0;
-    const delimiter = /[\n,]/;
+    let delimiter = /[\n,]/;
+    if (input.startsWith("//")) {
+        const parts = input.split("\n");
+        //below line is to add "\" before special character like * + ? so they dont throw error while creating regexExp
+        const escapedDelimiter = parts[0].slice(2).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        delimiter = new RegExp(`${escapedDelimiter}|\\n`); //replace delimeter with custom
+        input = parts.slice(1).join("\n"); //this line is to keep below code consistent
+    }
     const numbers = input.split(delimiter).map(Number);
     return numbers.reduce((sum, c) => sum + c , 0);
 }
